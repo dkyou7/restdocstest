1436cc2d1b5b97147319761eeea70db8a9432881
youdongkwan <dkyou7@naver.com>
2021-06-23 10:38:06 +0900

    [Feature] 프로젝트를 최초로 생성하였습니다. - 의존성 추가    - web    - lombok    - spring rest docs

---
+++ src/main/java/com/example/restdocstest/RestdocstestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restdocstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestdocstestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestdocstestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restdocstest/RestdocstestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restdocstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestdocstestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
